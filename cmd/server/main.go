0bf4aa41ba6245fc72f6515e9dda9b3dc8c92675
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2026-01-09 16:11:06 -0300

    setup simple API

---
+++ cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "atlas/routes"
+
+func main() {
+	routes.Start()
+}
---
+++ routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := router.Run()
+
+	if err != nil {
+    panic(err)
+	}
+}
